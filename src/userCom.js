b3b5815068cce937a194c719952f0c891fcb5c26
raju <nary%@admin>
2024-03-02 15:53:33 +0530

    feature

--- src/userCom.js
+++ src/userCom.js
@@ -9,16 +9,18 @@ const UserPage = () => {
     const [currentPage, setCurrentPage] = useState(0);
     const [pageSize, setPageSize] = useState(5);
     const [editingUserId, setEditingUserId] = useState(null);
-   
+    const [sortColumn, setSortColumn] = useState('userId'); // Default sorting column
+    const [sortOrder, setSortOrder] = useState('asc'); // Default sorting order
 
     useEffect(() => {
         fetchData();
-    }, [currentPage, pageSize]); // Fetch data when currentPage or pageSize changes
+    }, [currentPage, pageSize, sortColumn, sortOrder]);
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(`http://localhost:8090/pagenation?page=${currentPage}&size=${pageSize}`);
+            const response = await axios.get(`http://localhost:8090/pagedAndSorted?columnName=${sortColumn}&sortOrder=${sortOrder}&page=${currentPage}&size=${pageSize}`);
             setUserData(response.data);
+            console.log(userData);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -33,6 +35,7 @@ const UserPage = () => {
     const handleNextPage = () => {
         setCurrentPage(currentPage + 1);
     };
+
     const handleEdit = (userId) => {
         setEditingUserId(userId);
     };
@@ -56,25 +59,48 @@ const UserPage = () => {
         }
     };
 
+    const handleSort = (columnName) => {
+        // If clicking on the same column, toggle the sort order
+        if (columnName === sortColumn) {
+            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+        } else {
+            // If clicking on a different column, set new sort column and default to ascending order
+            setSortColumn(columnName);
+            setSortOrder('asc');
+        }
+        // Reset currentPage to 0 when sorting
+        setCurrentPage(0);
+    };
+
+    const handleFilter = async () => {
+        // Reset currentPage to 0 when filtering
+        setCurrentPage(0);
+        try {
+            const response = await axios.get(`http://localhost:8090/sorted?columnName=${sortColumn}&sortOrder=${sortOrder}&page=${currentPage}&size=${pageSize}`);
+            setUserData(response.data);
+           
+        } catch (error) {
+            console.error('Error fetching filtered data:', error);
+        }
+    };
+
     return (
         <div>
-           
            <h2 className='text-center mt-4 ' style={{color:'tomato'}}>USER DETAILS</h2>
-           
-         <Link to="/user" className='btn btn-primary'>Add User</Link>
+           <Link to="/user" className='btn btn-primary'>Add User</Link>
             <table className='table mt-2'>
                 <thead>
                     <tr>
-                        <th>User ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Age</th>
-                        <th>Phone Number</th>
+                        <th onClick={() => handleSort('userId')}>User ID</th>
+                        <th onClick={() => handleSort('userFirstName')}>First Name</th>
+                        <th onClick={() => handleSort('userLastName')}>Last Name</th>
+                        <th onClick={() => handleSort('userAge')}>Age</th>
+                        <th onClick={() => handleSort('userPhoneNo')}>Phone Number</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {userData.map(user => (
+                {userData && userData.map(user => (
                         <tr key={user.userId}>
                             <td>{user.userId}</td>
                             <EditableCell
